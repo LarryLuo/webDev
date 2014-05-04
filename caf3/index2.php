5f37086ed4fcabd9fcf0495483b1be7f3d9aafad
Larry'Lap <luoyuhang89@gmail.com>
2014-05-04 14:36:54 +0800

    1.modified index page 2.modified some pic source

--- caf3/index2.php
+++ caf3/index2.php
@@ -20,6 +20,7 @@
 		
 		<script type="text/javascript" src="css/scripts/jquery-1.11.0.min.js"></script>
     	<script type="text/javascript" src="css/scripts/jquery.nivo.slider.js"></script>
+    	<script type="text/javascript" src="css/scripts/wrapper.js"></script>
     	<script type="text/javascript">
     		$(window).load(function() {
         	$('#slider').nivoSlider();
@@ -35,19 +36,19 @@
 		<div id="container">
 			<div id="banner">
 				<div id="bannerButtons">
-					<div id="bannerButton"><a href="#"><img src="css/images/b_button 1a.png" onmouseover="this.src='css/images/b_button 1b.png'" onmouseout="this.src='css/images/b_button 1a.png'" /></a></div>
-					<div id="bannerButton"><a href="#"><img src="css/images/b_button 2a.png" onmouseover="this.src='css/images/b_button 2b.png'" onmouseout="this.src='css/images/b_button 2a.png'" /></a></div>
-					<div id="bannerButton"><a href="#"><img src="css/images/b_button 3a.png" onmouseover="this.src='css/images/b_button 3b.png'" onmouseout="this.src='css/images/b_button 3a.png'" /></a></div>
+					<div id="bannerButton"><a href="wrapper.php?sid=1&mid=1"><img src="css/images/b_button 1a.png" onmouseover="this.src='css/images/b_button 1b.png'" onmouseout="this.src='css/images/b_button 1a.png'" /></a></div>
+					<div id="bannerButton"><a href="wrapper.php?sid=5&mid=2"><img src="css/images/b_button 2a.png" onmouseover="this.src='css/images/b_button 2b.png'" onmouseout="this.src='css/images/b_button 2a.png'" /></a></div>
+					<div id="bannerButton"><a href="wrapper.php?sid=8&mid=3"><img src="css/images/b_button 3a.png" onmouseover="this.src='css/images/b_button 3b.png'" onmouseout="this.src='css/images/b_button 3a.png'" /></a></div>
 				</div>
 				
 				<div class="clearfloat"></div>
 				<div id="sliderBar">
 					<div class="slider-wrapper theme-default">
             			<div id="slider" class="nivoSlider">
-                			<img src="css/images/banner.png" data-thumb="css/images/banner.png" alt="" />
-                			<img src="css/images/banner.png" data-thumb="css/images/banner.png" alt="" title="This is an example of a caption" />
-                			<img src="css/images/banner.png" data-thumb="css/images/banner.png" alt="" data-transition="slideInLeft" />
-                			<img src="css/images/banner.png" data-thumb="css/images/banner.png" alt="" title="#htmlcaption" />
+                			<img src="css/images/pic_mov1.jpg" data-thumb="css/images/pic_mov1.jpg" alt="" />
+                			<img src="css/images/pic_mov1.jpg" data-thumb="css/images/pic_mov1.jpg" alt="" title="" />
+                			<img src="css/images/pic_mov1.jpg" data-thumb="css/images/pic_mov1.jpg" alt="" data-transition="slideInLeft" />
+                			<img src="css/images/pic_mov1.jpg" data-thumb="css/images/pic_mov1.jpg" alt="" title="" />
             			</div>
 			        </div>
 				</div>
@@ -61,14 +62,14 @@
 					</div>
 
 					<div id="navButtons">
-						<div id="navButton"><a href="#"><img src="css/images/e_button 1a.png" onmouseover="this.src='css/images/e_button 1b.png'" onmouseout="this.src='css/images/e_button 1a.png'" /></a></div>
-						<div id="navButton"><a href="#"><img src="css/images/e_button 2a.png" onmouseover="this.src='css/images/e_button 2b.png'" onmouseout="this.src='css/images/e_button 2a.png'" /></a></div>
-						<div id="navButton"><a href="#"><img src="css/images/e_button 3a.png" onmouseover="this.src='css/images/e_button 3b.png'" onmouseout="this.src='css/images/e_button 3a.png'" /></a></div>
+						<div id="navButton" onClick="Page_jump('sid=7&mid=2')"><img src="css/images/e_button 1a.png" onmouseover="this.src='css/images/e_button 1b.png'" onmouseout="this.src='css/images/e_button 1a.png'" /></div>
+						<div id="navButton" onClick="Page_jump('sid=8&mid=3')"><img src="css/images/e_button 2a.png" onmouseover="this.src='css/images/e_button 2b.png'" onmouseout="this.src='css/images/e_button 2a.png'" /></div>
+						<div id="navButton" onClick="Page_jump('sid=9&mid=3')"><img src="css/images/e_button 3a.png" onmouseover="this.src='css/images/e_button 3b.png'" onmouseout="this.src='css/images/e_button 3a.png'" /></div>
 					</div>
 					<div id="navButtons">
-						<div id="navButton"><a href="#"><img src="css/images/e_button 4a.png" onmouseover="this.src='css/images/e_button 4b.png'" onmouseout="this.src='css/images/e_button 4a.png'" /></a></div>
-						<div id="navButton"><a href="#"><img src="css/images/e_button 5a.png" onmouseover="this.src='css/images/e_button 5b.png'" onmouseout="this.src='css/images/e_button 5a.png'" /></a></div>
-						<div id="navButton"><a href="#"><img src="css/images/e_button 6a.png" onmouseover="this.src='css/images/e_button 6b.png'" onmouseout="this.src='css/images/e_button 6a.png'" /></a></div>
+						<div id="navButton" onClick="Page_jump('sid=3&mid=1')"><img src="css/images/e_button 4a.png" onmouseover="this.src='css/images/e_button 4b.png'" onmouseout="this.src='css/images/e_button 4a.png'" /></div>
+						<div id="navButton" onClick="Page_jump('sid=12&mid=3')"><img src="css/images/e_button 5a.png" onmouseover="this.src='css/images/e_button 5b.png'" onmouseout="this.src='css/images/e_button 5a.png'" /></div>
+						<div id="navButton" onClick="Page_jump('sid=13&mid=4')"><img src="css/images/e_button 6a.png" onmouseover="this.src='css/images/e_button 6b.png'" onmouseout="this.src='css/images/e_button 6a.png'" /></div>
 					</div>
 
 				</div>
@@ -78,7 +79,9 @@
 				<div id="news">
 					<div class="clearfloat"></div>
 					<div id="newsContent">
+						<div class="clearfloat"></div>
 						<div class="newsLeft">
+							<div class="newsList">
 							<ul>
 							<?php
 								$sql="SELECT * FROM p_newsbase ORDER BY id DESC LIMIT 0,8";
@@ -87,7 +90,7 @@
 							
 							?>
 							<li>	
-								<a href="#">
+								<a href="wrapper.php?sid=5&mid=2&nid=<?=$row['id']?>">
 									<?=$row['title']?>
 								</a>
 								<span style="float:right">
@@ -97,7 +100,28 @@
 							<?php
 								}
 							?>
-						</ul>
+							</ul>
+							</div>
+<!--							
+								<ul id="tab_nav">
+									<li><a href="#t_1"><img src="css/images/news_button 1a.png"></a></li>
+									<li><a href="#t_2"><img src="css/images/news_button 2a.png"></a></li>
+									<li><a href="#t_3"><img src="css/images/news_button 3a.png"></a></li>
+								</ul>
+								<div id="tab_content">
+									<div id="t_1">
+									</div>
+
+									<div id="t_2">
+									</div>
+
+									<div id="t_3">
+									</div>
+								</div>
+-->							
+
+
+
 						</div>
 
 						<div id="newsRight">
@@ -142,8 +166,7 @@
 
 						<div id="mediaPicArea">	
 							<div id="mediaPic">
-								<a href=" <?=$row['url']?> ">
-									<span style="position:absolute"><?=$row['title']?></span>
+								<a href="<?=$row['url']?>" target="_blank">
 									<img src=" <?=$row['sample']?> ">
 								</a>
 								
@@ -167,7 +190,7 @@
 
 						<div id="contactLeftDown">
 							<div id="contactLeftDown1"></div>
-							<div id="contactLeftDown2"></div>
+							<div id="contactLeftDown2" style="display:none"></div>
 						</div>
 
 					</div>
@@ -177,7 +200,7 @@
 				</div>
 
 				<div class="clearfloat"></div>
-
+<!--
 				<div id="partner">
 					<?php
 						$sql="SELECT * FROM p_partner ORDER BY id DESC LIMIT 0,21";
@@ -187,14 +210,14 @@
 					?>
 					<div id="partnerLogo">	
 						<a href=" <?=$row['url']?> ">
-							<span style="position:absolute"><?=$row['name']?></span><img src=" <?=$row['image']?> ">
+							<span style="position:absolute"><?=$row['name']?></span><img style="width:100px;height:56px" src=" <?=$row['image']?> ">
 						</a>
 					</div>
 					<?php
 						}
 					?>
 				</div>
-
+-->
 				<div class="clearfloat"></div>
 
 			</div>
