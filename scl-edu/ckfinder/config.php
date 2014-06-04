368fee8535b19e514070a00c692331da5def3e0d
LarryCompany <luoyuhang89@gmail.com>
2014-06-04 13:08:37 +0800

    1.modified config of ckfinder

--- scl-edu/ckfinder/config.php
+++ scl-edu/ckfinder/config.php
@@ -60,7 +60,7 @@ Examples:
 
 ATTENTION: The trailing slash is required.
 */
-$baseUrl = '/ckfinder/userfiles/';
+$baseUrl = 'http://www.cdcaf.com/scl-edu/ckfinder/userfiles/';
 
 /*
 $baseDir : the path to the local directory (in the server) which points to the
